f9e483bfce70025dea94849e343ec630c7a473f5
Vince Harresh Rodriguez <vinceharresh@gmail.com>
2020-03-12 12:26:11 +0800

    post,profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,16 +4,22 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="/Images/freeCodeGram.jpg" class="rounded-circle" alt="">
+            <img src="/storage/{{$user->profile->image}}" class="rounded-circle w-100" alt="">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="#">Add new Post</a>
-
+                @can('update',$user->profile)
+                    <a href="/p/create">Add new Post</a>
+                @endcan
             </div>
+
+            @can('update',$user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
+
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
@@ -23,9 +29,14 @@
         </div>
     </div>
     <div class="row pt-5">
-        <div class="col-4"><img class="w-100" src="\Images\84217667_548898739065440_6328985649004661540_n.jpg" alt=""></div>
-        <div class="col-4"><img class="w-100" src="\Images\88374782_543733416501650_6638052850750298038_n.jpg" alt=""></div>
-        <div class="col-4"><img class="w-100" src="\Images\89485502_198087498118276_8717397081789116228_n.jpg" alt=""></div>
+        @foreach ($user->posts as $post)
+            <div class="col-4">
+            <a href="/p/{{$post->id}}">
+                    <img class="w-100 pb-4" src="/storage/{{$post->image}}" alt="">
+                </a>
+            </div>
+        @endforeach
     </div>
+</div>
 </div>
 @endsection
