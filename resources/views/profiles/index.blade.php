f152c4f9d8decc5bcccb4df5bec35674c098d979
Vince Harresh Rodriguez <vinceharresh@gmail.com>
2020-03-13 14:40:36 +0800

    follow,profile,telescope

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,7 +8,12 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{$user->username}}</h1>
+                <div class="d-flex align-items-center ">
+                    <div class="h4">{{$user->username}}</div>
+
+                <follow-button user-id="{{$user->id}}" follows="{{$follows}}"></follow-button>
+
+                </div>
                 @can('update',$user->profile)
                     <a href="/p/create">Add new Post</a>
                 @endcan
@@ -20,8 +25,8 @@
 
             <div class="d-flex">
                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
-                <div class="pr-5"><strong>23k</strong> followers</div>
-                <div class="pr-5"><strong>212</strong> following</div>
+                <div class="pr-5"><strong>{{$user->profile->followers->count()}}</strong> followers</div>
+                <div class="pr-5"><strong>{{$user->following->count()}}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div>{{$user->profile->description}}</div>
