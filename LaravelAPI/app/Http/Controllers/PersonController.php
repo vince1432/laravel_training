f2a6e0474ad46ca3928c3d8215f08847355be111
vince <vinceharresh@gmail.com>
2020-03-18 10:11:34 +0800

    recreate

--- LaravelAPI/app/Http/Controllers/PersonController.php
+++ LaravelAPI/app/Http/Controllers/PersonController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Person;
+use App\Http\Resources\PersonResource;
+use App\Http\Resources\PersonResourceCollection;
+
 class PersonController extends Controller
 {
-    public function show(Person $person)
+    public function show(Person $person) : PersonResource
     {
-        return $person;
+        return new PersonResource($person);
+    }
+
+    public function index() : PersonResourceCollection
+    {
+        return new PersonResourceCollection(Person::paginate());
     }
 
 }
